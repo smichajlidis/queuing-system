9981bc43abeae469671bfbfa2b9584eea962fc1c
smichajlidis <smichajlidis@gmail.com>
2024-01-04 15:26:21 +0100

    add a queue, window and pick up 3 tickets

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <queue>
+#include "include/QueuesManager.hpp"
+#include "include/WindowsManager.hpp"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    QueuesManager queues;
+    WindowsManager windows;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    std::queue<int> a {};
+
+    queues.addQueue(a);
+    
+    queues.pickATicket(0);
+    queues.pickATicket(0);
+    queues.pickATicket(0);
+
+    windows.addWindow(sf::VideoMode(200, 200), "SFML works!");
+
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
+
+    // while (window.isOpen()) {
+    //     sf::Event event;
+    //     while (window.pollEvent(event)) {
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //     }
+
+    //     window.clear();
+    //     //window.draw(shape);
+    //     window.display();
+    // }
 
     return 0;
 }
