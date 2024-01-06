c41888e469acf423e7998ec123b29c910306c29e
smichajlidis <smichajlidis@gmail.com>
2024-01-06 18:33:52 +0100

    define getATicket method

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -1,8 +1,16 @@
 #include "../include/Queue.hpp"
 
 void Queue::pickATicket() {
-    queue.push(queueLength+1);
+    queue.push_back(queueLength+1);
     ++queueLength;
+}
+
+size_t Queue::size() const {
+    return queue.size();
+}
+
+int Queue::getATicket(int pos) const {
+    return queue.at(pos);
 }
 
 Queue::Queue() {}
