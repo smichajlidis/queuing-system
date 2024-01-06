759a92bdc5df3ee9e041ede04e60c517dae8d6a3
smichajlidis <smichajlidis@gmail.com>
2024-01-06 16:10:11 +0100

    define run(), isOpen() universal methods

--- src/Window.cpp
+++ src/Window.cpp
@@ -6,3 +6,15 @@ Window::Window(const sf::VideoMode& mode, const std::string& title)
 void Window::addRelatedQueue(const Queue& relatedQueue) {
     relatedQueues.push_back(std::make_shared<Queue>(relatedQueue));
 }
+
+bool Window::isOpen() const {
+    return window.isOpen();
+}
+
+void Window::run() {
+    while (window.isOpen()) {
+        processEvents();
+        update();
+        render();
+    }
+}
