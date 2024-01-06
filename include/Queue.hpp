799c3426c2816d3facff54da98180617da050a93
smichajlidis <smichajlidis@gmail.com>
2024-01-06 19:24:59 +0100

    add signature atribute and getSignature method

--- include/Queue.hpp
+++ include/Queue.hpp
@@ -6,15 +6,17 @@
 class Queue {
     std::vector<int> queue {};
     std::string topic {};
+    char signature {};
     int queueLength {0};
     int priority = 0;
 public:
     void pickATicket(); // take a new ticket
     size_t size() const;
+    char getSignature() const;
     int getATicket(int pos) const; // get a name of existing ticket
     Queue();
-    Queue(std::string topic);
-    Queue(std::string topic, int priority);
+    Queue(std::string topic, char signature);
+    Queue(std::string topic, char signature, int priority);
     ~Queue();
 };
 
