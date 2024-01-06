ef02bbc2b7305eeab5beb53f24e1fe94e565d341
smichajlidis <smichajlidis@gmail.com>
2024-01-06 16:46:31 +0100

    delete display() and update() declaration and add title atribut

--- include/Window.hpp
+++ include/Window.hpp
@@ -2,6 +2,7 @@
 #define _WINDOW_HPP_
 #include <memory>
 #include <vector>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include "Queue.hpp"
 
@@ -9,13 +10,12 @@ class Window {
 protected:
     sf::RenderWindow window;
     std::vector<std::shared_ptr<Queue>> relatedQueues;
+    std::string title;
 public:
     virtual void addRelatedQueue(const Queue& relatedQueue);
     virtual bool isOpen() const;
-    virtual void display() const = 0;
     virtual void run();
     virtual void processEvents() = 0;
-    virtual void update() = 0;
     virtual void render() = 0;
     Window(const sf::VideoMode& mode, const std::string& title);
     ~Window() {};
