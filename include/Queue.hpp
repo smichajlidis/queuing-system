84888dbff733cc59c83de5e71740edccc3465c5a
smichajlidis <smichajlidis@gmail.com>
2024-01-06 18:31:26 +0100

    change queue to vector

--- include/Queue.hpp
+++ include/Queue.hpp
@@ -1,15 +1,17 @@
 #ifndef _QUEUE_HPP_
 #define _QUEUE_HPP_
-#include <queue>
+#include <vector>
 #include <string>
 
 class Queue {
-    std::queue<int> queue {};
+    std::vector<int> queue {};
     std::string topic {};
     int queueLength {0};
     int priority = 0;
 public:
-    void pickATicket();
+    void pickATicket(); // take a new ticket
+    size_t size() const;
+    int getATicket(int pos) const; // get a name of existing ticket
     Queue();
     Queue(std::string topic);
     Queue(std::string topic, int priority);
