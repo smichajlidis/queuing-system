14773fe3f485218ab7471b3375632ce1f9211fd5
smichajlidis <smichajlidis@gmail.com>
2024-01-06 16:49:00 +0100

    define processEvents, render and constructor to get two buttons on the screen

--- src/Station.cpp
+++ src/Station.cpp
@@ -1,20 +1,33 @@
 #include "../include/Station.hpp"
 
-Station::Station(const sf::VideoMode& mode, const std::string& title)
-    : Window(mode, title) {}
+Station::Station(const sf::VideoMode& mode, const std::string& newTitle)
+    : Window(mode, newTitle), nextButton (sf::Vector2f(150, 50)),
+    confirmButton (sf::Vector2f(150, 50)) {
 
-void Station::display() const {
-    
+    title = newTitle;
+
+    nextButton.setFillColor(sf::Color::Green);
+    nextButton.setPosition(25, 450);
+
+    confirmButton.setFillColor(sf::Color::Blue);
+    confirmButton.setPosition(225, 450);
+
 }
 
 void Station::processEvents() {
-
-}
-
-void Station::update() {
-
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+    }
 }
 
 void Station::render() {
+    window.clear();
 
+    window.draw(nextButton);
+    window.draw(confirmButton);
+
+    window.display();
 }
