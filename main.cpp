27fe4b10c83918e94dce585f2108b6a536c064e8
smichajlidis <smichajlidis@gmail.com>
2024-01-07 19:06:24 +0100

    delete everything except QueuesManager include, main and QueuesManager object creation

--- main.cpp
+++ main.cpp
@@ -1,25 +1,5 @@
-#include <SFML/Graphics.hpp>
 #include "include/QueuesManager.hpp"
 
 int main() {
-
     QueuesManager queuesManager {};
-
-    
-    //sf::CircleShape shape(100.f);
-    //shape.setFillColor(sf::Color::Green);
-
-    // while (window.isOpen()) {
-    //     sf::Event event;
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-
-    //     window.clear();
-    //     //window.draw(shape);
-    //     window.display();
-    // }
-
-    return 0;
 }
