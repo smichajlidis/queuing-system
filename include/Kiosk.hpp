948b9918d63943159e9772f902dd08c4396c45de
smichajlidis <smichajlidis@gmail.com>
2024-01-08 21:34:49 +0100

    create Kiosk.hpp

---
+++ include/Kiosk.hpp
@@ -0,0 +1,19 @@
+#ifndef _KIOSK_HPP_
+#define _KIOSK_HPP_
+#include "Window.hpp"
+#include <SFML/Graphics.hpp>
+#include "Queue.hpp"
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+class Kiosk: public Window {
+private:
+    std::unordered_map<std::shared_ptr<Queue>, sf::RectangleShape> topics;
+public:
+    virtual void processEvents() override;
+    virtual void render() override;
+    Kiosk(const sf::VideoMode& mode, const std::string& title, const std::vector<std::shared_ptr<Queue>>& queues);    
+};
+
+#endif
