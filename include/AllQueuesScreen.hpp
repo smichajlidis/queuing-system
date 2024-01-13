08b3173d3da608b2346360f54b2cea58e065925e
smichajlidis <smichajlidis@gmail.com>
2024-01-13 15:15:01 +0100

    change activeStations from Window class vector to Station class vector

--- include/AllQueuesScreen.hpp
+++ include/AllQueuesScreen.hpp
@@ -1,6 +1,7 @@
 #ifndef _ALLQUEUESSCREEN_HPP_
 #define _ALLQUEUESSCREEN_HPP_
 #include "Window.hpp"
+#include "Station.hpp"
 #include <SFML/Graphics.hpp>
 #include "Queue.hpp"
 #include <vector>
@@ -8,11 +9,11 @@
 
 class AllQueuesScreen: public Window {
 private:
-    std::vector<std::shared_ptr<Window>> activeStations; // vector of all active stations for display datas
+    std::vector<std::shared_ptr<Station>> activeStations; // vector of all active stations for display datas
 public:
     virtual void processEvents() override;
     virtual void render() override;
-    AllQueuesScreen(const sf::VideoMode& mode, const std::string& title, const std::vector<std::shared_ptr<Queue>>& relatedQueues, const std::vector<std::shared_ptr<Window>>& stations);    
+    AllQueuesScreen(const sf::VideoMode& mode, const std::string& title, const std::vector<std::shared_ptr<Queue>>& relatedQueues, const std::vector<std::shared_ptr<Station>>& stations);    
 };
 
 #endif
