51946a1b6d0e4b1a6f8ca1d98c4786718543db91
smichajlidis <smichajlidis@gmail.com>
2024-01-22 11:25:55 +0100

    define stationsCreator method

--- src/QueuesManager.cpp
+++ src/QueuesManager.cpp
@@ -1,62 +1,16 @@
 #include "../include/QueuesManager.hpp"
 
 QueuesManager::QueuesManager() {
-
+    //create queues
     int numberOfQueues {systemCreator("queues",12)};
-
     for (int i = 0; i < numberOfQueues; ++i)
         addQueue(queuesCreator());
-
+    //create stations
     int numberOfStations {systemCreator("stations",9)};
-    // create examples queues
-    std::shared_ptr<Queue> queue = std::make_shared<Queue>("Example", 'E');
-    std::shared_ptr<Queue> queue2 = std::make_shared<Queue>();
-    std::shared_ptr<Queue> queue3 = std::make_shared<Queue>("New example", 'N');
-    std::shared_ptr<Queue> queue4 = std::make_shared<Queue>("Another example", 'A');
-
-    // queue->pickATicket();
-    // queue->pickATicket();
-    // queue->pickATicket();
-    // add queues to vectors of all queues
-    // addQueue(queue);
-    // addQueue(queue2);
-    // addQueue(queue3);
-    // addQueue(queue4);
-    // create vector of relatedQueues for a station
-    std::vector<std::shared_ptr<Queue>> relatedQueues;
-    relatedQueues.push_back(queue);
-    relatedQueues.push_back(queue4);
-    std::vector<std::shared_ptr<Queue>> relatedQueues2;
-    relatedQueues2.push_back(queue);
-    std::vector<std::shared_ptr<Queue>> relatedQueues3;
-    relatedQueues3.push_back(queue2);
-    relatedQueues3.push_back(queue3);
-    relatedQueues3.push_back(queue4);
-    std::vector<std::shared_ptr<Queue>> relatedQueues4;
-    relatedQueues4.push_back(queue2);
-    std::vector<std::shared_ptr<Queue>> relatedQueues5;
-    relatedQueues5.push_back(queue);
-    relatedQueues5.push_back(queue2);
-    relatedQueues5.push_back(queue3);
-    relatedQueues5.push_back(queue4);
-    // create some stations for testing and add it to the vector
-    std::shared_ptr<Station> station = std::make_shared<Station>(sf::VideoMode(400, 600), "Station 1", relatedQueues);
-    addWindow(station);
-    std::shared_ptr<Station> station2 = std::make_shared<Station>(sf::VideoMode(400, 600), "Station 2", relatedQueues2);
-    addWindow(station2);
-    std::shared_ptr<Station> station3 = std::make_shared<Station>(sf::VideoMode(400, 600), "Station 3", relatedQueues3);
-    addWindow(station3);
-    std::shared_ptr<Station> station4 = std::make_shared<Station>(sf::VideoMode(400, 600), "Station 4", relatedQueues4);
-    addWindow(station4);
-    std::shared_ptr<Station> station5 = std::make_shared<Station>(sf::VideoMode(400, 600), "Station 5", relatedQueues5);
-    addWindow(station5);
-    // create vector of existing stations for allQueuesScreen
-    std::vector<std::shared_ptr<Station>> stations;
-    stations.push_back(station);
-    stations.push_back(station2);
-    stations.push_back(station3);
-    stations.push_back(station4);
-    stations.push_back(station5);
+    for (int i = 0; i < numberOfStations; ++i) {
+        std::cout << "\t<<STATION CREATOR>>\n" << std::endl;
+        addStation(stationsCreator());
+    }
 
     std::shared_ptr<Kiosk> kiosk = std::make_shared<Kiosk>(sf::VideoMode(400,600), "Kiosk nr 1", queues);
     addWindow(kiosk);
@@ -145,4 +99,48 @@ std::shared_ptr<Queue> QueuesManager::queuesCreator() const {
         signature = std::toupper(signature);
     return std::make_shared<Queue>(topic, signature);
     
+}
+
+void QueuesManager::addStation(std::shared_ptr<Station> station) {
+    stations.push_back(station);
+}
+
+std::shared_ptr<Station> QueuesManager::stationsCreator() {
+    size_t choice = 0;
+    std::vector<std::shared_ptr<Queue>> allQueues = queues;
+    std::vector<std::shared_ptr<Queue>> relatedQueues;
+    do {
+        int count = 1;
+        for (auto queue: allQueues) {
+            std::cout << "\t" << count << ".\t" << allQueues.at(count-1)->getTopic() << std::endl;
+            ++count;
+        }
+        if (!relatedQueues.empty())
+            std::cout << "\t0.\tAll related queues have related" << std::endl; 
+        std::cout << "\nWhich queues do you want to relate with?" << std::endl;
+        try {
+            std::cin >> choice;
+            if (std::cin.fail() || choice > allQueues.size() || (choice == 0 && relatedQueues.empty())) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                throw InvalidAmountException(allQueues.size());
+            }
+            else if (choice) {
+                relatedQueues.push_back(allQueues.at(choice-1));
+                auto toRemove = allQueues.begin() + (choice - 1);
+                allQueues.erase(toRemove);
+            }
+        } catch (const InvalidAmountException &ex) {
+            std::cerr << ex.what() << std::endl;
+        }
+    } while (choice);
+
+    std::cout << "What is the name of the station?" << std::endl;
+    std::string name;
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    std::shared_ptr<Station> station = std::make_shared<Station>(sf::VideoMode(400, 600), name, relatedQueues);
+    addWindow(station);
+    std::cout << name << " created\n" << std::endl;
+    return station;
 }
