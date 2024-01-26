999f79b13a20e5c85a43efa46ad2bec6a17e33b2
Serfer <bartosz.krzykk@gmail.com>
2024-01-26 14:50:50 +0100

    full review

---
+++ build/CMakeFiles/queuing-system.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bartek/Documents/projekty/queuing-system/main.cpp" "CMakeFiles/queuing-system.dir/main.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/main.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/AllQueuesScreen.cpp" "CMakeFiles/queuing-system.dir/src/AllQueuesScreen.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/AllQueuesScreen.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/Animator.cpp" "CMakeFiles/queuing-system.dir/src/Animator.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/Animator.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/Kiosk.cpp" "CMakeFiles/queuing-system.dir/src/Kiosk.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/Kiosk.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/Queue.cpp" "CMakeFiles/queuing-system.dir/src/Queue.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/Queue.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/QueuesManager.cpp" "CMakeFiles/queuing-system.dir/src/QueuesManager.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/QueuesManager.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/Station.cpp" "CMakeFiles/queuing-system.dir/src/Station.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/Station.cpp.o.d"
+  "/home/bartek/Documents/projekty/queuing-system/src/Window.cpp" "CMakeFiles/queuing-system.dir/src/Window.cpp.o" "gcc" "CMakeFiles/queuing-system.dir/src/Window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/queuing-system.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queuing-system.dir/main.cpp.o"
+  "CMakeFiles/queuing-system.dir/main.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/AllQueuesScreen.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/AllQueuesScreen.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/Animator.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/Animator.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/Kiosk.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/Kiosk.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/Queue.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/Queue.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/QueuesManager.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/QueuesManager.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/Station.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/Station.cpp.o.d"
+  "CMakeFiles/queuing-system.dir/src/Window.cpp.o"
+  "CMakeFiles/queuing-system.dir/src/Window.cpp.o.d"
+  "queuing-system"
+  "queuing-system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/queuing-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
