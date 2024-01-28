c374ba8efcde0578daa8c6c5c482318f103abf7b
smichajlidis <smichajlidis@gmail.com>
2024-01-28 20:47:31 +0100

    change if-else to ternary expression

--- src/Animator.cpp
+++ src/Animator.cpp
@@ -3,10 +3,7 @@
 void Animator::notConfirmedTicketAnimation(sf::Text& ticket) {
 
     if ((clock.getElapsedTime()).asSeconds() >= 0.6f) {
-        if (color == sf::Color::Black)
-            color = sf::Color::White;
-        else
-            color = sf::Color::Black;
+        color = (color == sf::Color::Black) ? sf::Color::White : sf::Color::Black;
         start();
     }
     ticket.setFillColor(color);
