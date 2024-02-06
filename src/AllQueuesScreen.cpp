4b7bbb2ef1ff5531144fb6804edd38817009388f
smichajlidis <smichajlidis@gmail.com>
2024-02-06 14:35:59 +0100

    Set proper distances in list of stations
    There was problems with distances in list of station when >4. Now it is fixed.

--- src/AllQueuesScreen.cpp
+++ src/AllQueuesScreen.cpp
@@ -7,7 +7,7 @@ AllQueuesScreen::AllQueuesScreen(const sf::VideoMode& mode, const std::string& t
     window_grid.setRows(18, 
     20.0, 30.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0,
     30.0, 30.0, 20.0, 20.0, 20.0, 20.0, 20.0);
-    window_grid.setColumns(7, 20.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0);
+    window_grid.setColumns(10, 20.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0);
 
 }
 
@@ -45,26 +45,26 @@ void AllQueuesScreen::render() {
     window.draw(text);
 
     column = 1;
-    int count = 0;
+    int line {1};
 
     for (size_t i = 1; i <= active_stations.size(); ++i) {
         sf::Text station_topic(active_stations.at(i-1)->getTitle(), font, 18);
         station_topic.setFillColor(sf::Color::White);
-        station_topic.setPosition(window_grid.getColumn(column), window_grid.getRow(12+i));
+        station_topic.setPosition(window_grid.getColumn(column), window_grid.getRow(12+line));
         window.draw(station_topic);
 
         sf::Text actual_ticket(active_stations.at(i-1)->getCurrentTicket(), font, 18);
-        actual_ticket.setPosition(window_grid.getColumn(column+2), window_grid.getRow(12+i));
+        actual_ticket.setPosition(window_grid.getColumn(column+3), window_grid.getRow(12+line));
         // animate if the ticket is not confirmed yet
         if (active_stations.at(i-1)->getWaitingForCurrentTicket()) {
             active_stations.at(i-1)->notConfirmedTicketAnimation(actual_ticket);
         }
         window.draw(actual_ticket);
         
-        ++count;
-        if (i % 3 == 0) {
+        ++line;
+        if (i % 4 == 0) {
             column += 4;
-            count = 0;
+            line = 1;
         }
     }
 
