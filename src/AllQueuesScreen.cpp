1ece9ffb512aabac058ff87bd218b4833a816d26
smichajlidis <smichajlidis@gmail.com>
2024-02-06 15:45:26 +0100

    Set (...) instead of following tickets when tickets are more than 10

--- src/AllQueuesScreen.cpp
+++ src/AllQueuesScreen.cpp
@@ -1,5 +1,6 @@
 #include "../include/AllQueuesScreen.hpp"
-#include <iostream>
+
+#include <algorithm>
 
 AllQueuesScreen::AllQueuesScreen(const sf::VideoMode& mode, const std::string& title, const std::vector<std::shared_ptr<Queue>>& related_queues, const std::vector<std::shared_ptr<Station>>& stations)
     : Window(mode, title, related_queues), active_stations (stations) {
@@ -7,7 +8,7 @@ AllQueuesScreen::AllQueuesScreen(const sf::VideoMode& mode, const std::string& t
     window_grid.setRows(18, 
     20.0, 30.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0,
     30.0, 30.0, 20.0, 20.0, 20.0, 20.0, 20.0);
-    window_grid.setColumns(10, 20.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0, 40.0);
+    window_grid.setColumns(10, 20.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0);
 
 }
 
@@ -28,10 +29,10 @@ void AllQueuesScreen::render() {
     int column {1};
 
     for (auto &queue: related_queues) {
-        size_t size = queue->getSize();
+        int size = static_cast<int>(queue->getSize());
         int last = queue->getLastInQueue();
-        for (size_t i = 1; i <= size; ++i) {
-            sf::Text number_text(queue->getSignature() + std::to_string(last), font, 18);
+        for (int i = 1; i <= std::min(size, 10); ++i) {
+            sf::Text number_text(((i < 10) ? queue->getSignature() + std::to_string(last) : "(...)"), font, 18);
             number_text.setFillColor(sf::Color::White);
             number_text.setPosition(window_grid.getColumn(column), window_grid.getRow(i+1));
             window.draw(number_text);
@@ -54,7 +55,7 @@ void AllQueuesScreen::render() {
         window.draw(station_topic);
 
         sf::Text actual_ticket(active_stations.at(i-1)->getCurrentTicket(), font, 18);
-        actual_ticket.setPosition(window_grid.getColumn(column+3), window_grid.getRow(12+line));
+        actual_ticket.setPosition(window_grid.getColumn(column+2), window_grid.getRow(12+line));
         // animate if the ticket is not confirmed yet
         if (active_stations.at(i-1)->getWaitingForCurrentTicket()) {
             active_stations.at(i-1)->notConfirmedTicketAnimation(actual_ticket);
@@ -63,7 +64,7 @@ void AllQueuesScreen::render() {
         
         ++line;
         if (i % 4 == 0) {
-            column += 4;
+            column += 3;
             line = 1;
         }
     }
